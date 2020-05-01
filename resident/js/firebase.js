a61b70fc4a4d72cf58cb083ea223c85841df0852
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-05-01 21:35:17 +0800

    add resident sort button

--- resident/js/firebase.js
+++ resident/js/firebase.js
@@ -13,9 +13,45 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 //Get Data
+
+var userDataRef = firebase.database().ref("building");
+userDataRef.once("value").then(
+    res => {
+        var districtBtn =
+            `<div class="actSortNav__btn" id="all-btn" onClick="sortByDistrict('全部', 'all')">
+        <div class="actSortNav__text" id="all-text">
+        全部
+        </div>
+        </div>`;
+        $('#sortNav').append(districtBtn);
+        res.forEach((districtSh, buildingIndex) => {
+            let districtInfo = districtSh.child("0").val();
+            console.log(districtInfo.district_chi, districtSh.key);
+            districtBtn =
+                `<div class="sortNav__btn" id="${districtSh.key}-btn" onClick="sortByDistrict('${districtInfo.district_chi}', '${districtSh.key}')">
+                    <div class="sortNav__text" id="${districtSh.key}-text">
+                    ${districtInfo.district_chi}
+                </div>
+                </div>`;
+            $('#sortNav').append(districtBtn);
+        })
+        var districtBtn =
+            `<div class="sortNav__btn" id="park-btn" onClick="sortByDistrict('公園', 'park')">
+        <div class="sortNav__text" id="park-text">
+        公園
+        </div>
+        </div>`;
+        $('#sortNav').append(districtBtn);
+    },
+    rej => {
+        console.log(rej);
+    }
+);
+
 var userDataRef = firebase.database().ref("resident");
 userDataRef.once("value").then(
     res => {
+        var districtBtn;
         res.forEach((characterSh, index) => {
             var character = characterSh.val();
             var characterString =
@@ -33,3 +69,46 @@ userDataRef.once("value").then(
         console.log(rej);
     }
 );
+var tempClassName = "all";
+function sortByDistrict(district, className) {
+    console.log(district, className, tempClassName);
+    $("#resident").empty();
+    document.getElementById(tempClassName + "-btn").className = 'sortNav__btn';
+    document.getElementById(tempClassName + "-text").className = 'sortNav__text';
+    document.getElementById(className + "-btn").className = 'actSortNav__btn';
+    document.getElementById(className + "-text").className = 'actSortNav__text';
+    var userDataRef = firebase.database().ref("resident");
+    userDataRef.once("value").then(
+        res => {
+            res.forEach((characterSh, index) => {
+                var character = characterSh.val();
+                if (district === '全部') {
+                    var characterString =
+                        `<div target="_blank" class="resident__box" id="characterId=${characterSh.key}" onclick="chooseCharacter(${characterSh.key})">` +
+                        `<div class="resident__box__character" id="${character.user_name}">` +
+                        `<img class="resident__box__character__image" src="image/${character.face}.png">` +
+                        `<div class="resident__box__character__title">${character.user_name}</div>` +
+                        `</div>` +
+                        `</div>` +
+                        `</a>`;
+                    $('#resident').append(characterString);
+                }
+                else if (character.district_CHI === district) {
+                    var characterString =
+                        `<div target="_blank" class="resident__box" id="characterId=${characterSh.key}" onclick="chooseCharacter(${characterSh.key})">` +
+                        `<div class="resident__box__character" id="${character.user_name}">` +
+                        `<img class="resident__box__character__image" src="image/${character.face}.png">` +
+                        `<div class="resident__box__character__title">${character.user_name}</div>` +
+                        `</div>` +
+                        `</div>` +
+                        `</a>`;
+                    $('#resident').append(characterString);
+                }
+            })
+        },
+        rej => {
+            console.log(rej);
+        }
+    );
+    tempClassName = className;
+}
