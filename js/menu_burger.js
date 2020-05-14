57556d6e7323dcb2a68b7af0f9e90aaba5d7af71
ngkwanlok <t106590052@ntut.org.tw>
2020-05-14 15:28:35 +0800

    redisign the phone top nav and add dark mode

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -1,8 +1,11 @@
 function menu_burger(x) {
+    x = document.getElementsByClassName("PhoneTop__mainnav__icon")[0];
     x.classList.toggle("change");
     if (PhoneTop__mainnav__container.style.display === "flex") {
         PhoneTop__mainnav__container.style.display = "none";
     } else {
         PhoneTop__mainnav__container.style.display = "flex";
     }
+    console.log(x);
+    console.log(document.getElementsByClassName("PhoneTop__mainnav__icon")[0]);
 }
