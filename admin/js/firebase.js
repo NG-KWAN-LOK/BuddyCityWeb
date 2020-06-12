fba8947ef79b8453b0cc5eb681383ab74f0774a8
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-06-12 20:45:16 +0800

    buildingList adding system on admin page done

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -28,11 +28,15 @@ async function readDatabase(pagechoose) {
         res.forEach((Item, index) => {
           //console.log(Item.getKey());
           var itemKey = Item.getKey();
-          //console.log(Item.val());
           itemValue = Item.val();
+          console.log(itemValue);
           var divContent = `<div>${itemKey}：<div>`;
           for (var title in itemValue) {
+            //console.log(itemValue[title]);
             divContent += `<div>${title}：${itemValue[title]}<div>`;
+            for (var value in itemValue[title]) {
+              divContent += `<div>${value}：${itemValue[title][value]}<div>`;
+            }
           }
           $("#admin__monitor").append(divContent);
         });
@@ -65,6 +69,278 @@ async function readDatabase(pagechoose) {
       }
     );
   }
+}
+async function newItemBuilding(districtId) {
+  console.log(districtId);
+  var DataRef = firebase.database().ref("/building/" + districtId);
+  var totalChild = 0;
+  await DataRef.once("value").then(
+    (res) => {
+      res.forEach((Item, index) => {
+        totalChild += 1;
+      });
+      return true;
+    },
+    (rej) => {
+      console.log(rej);
+      return true;
+    }
+  );
+  console.log(totalChild);
+  const form = document.forms["form"];
+  var Utilization = form.elements.Utilization.value;
+  var adress_chi = form.elements.adress_chi.value;
+  var adress_eng = form.elements.adress_eng.value;
+  var adress_jp = form.elements.adress_jp.value;
+  var district_chi = form.elements.district_chi.value;
+  var district_eng = form.elements.district_eng.value;
+  var district_jp = form.elements.district_jp.value;
+  var inside = form.elements.inside.value;
+  var name_chi = form.elements.name_chi.value;
+  var name_eng = form.elements.name_eng.value;
+  var name_jp = form.elements.name_jp.value;
+  var x = form.elements.x.value;
+  var y = form.elements.y.value;
+  var z = form.elements.z.value;
+  var インテリア = form.elements.インテリア.value;
+  var 內裝 = form.elements.內裝.value;
+  var 用途 = form.elements.用途.value;
+  var 用途_jp = form.elements.用途_jp.value;
+  if (
+    Utilization != "" &&
+    adress_chi != "" &&
+    adress_eng != "" &&
+    adress_jp != "" &&
+    district_chi != "" &&
+    district_eng != "" &&
+    district_jp != "" &&
+    inside != "" &&
+    name_chi != "" &&
+    name_eng != "" &&
+    name_jp != "" &&
+    インテリア != "" &&
+    內裝 != "" &&
+    用途 != "" &&
+    用途_jp != "" &&
+    x != "" &&
+    y != "" &&
+    z != ""
+  ) {
+    firebase
+      .database()
+      .ref("/building/" + districtId + "/" + totalChild)
+      .set({
+        Utilization: Utilization,
+        adress_chi: adress_chi,
+        adress_eng: adress_eng,
+        adress_jp: adress_jp,
+        district_chi: district_chi,
+        district_eng: district_eng,
+        district_jp: district_jp,
+        inside: inside,
+        name_chi: name_chi,
+        name_eng: name_eng,
+        name_jp: name_jp,
+        インテリア: インテリア,
+        內裝: 內裝,
+        用途: 用途,
+        用途_jp: 用途_jp,
+        x: x,
+        y: y,
+        z: z,
+      })
+      .then(function () {
+        alert("建立成功");
+      })
+      .catch(function () {
+        alert("伺服器發生錯誤 或者 你是黑客 ㄇㄌㄈㄎ！！");
+      });
+  } else {
+    alert("建立失敗！！！未有填寫全部資料");
+  }
+  $("#admin__monitor").empty();
+}
+function showEditBuildingForm(
+  districtId,
+  district_chi,
+  district_eng,
+  district_jp
+) {
+  $("#admin__monitor").empty();
+  var divContent = `<div class="admin__monitor__title">
+  增加新資料於居民名冊：
+</div>
+<div class="admin__monitor__subtitle">
+  請填寫以下表格（全部值必須填寫）
+</div>
+<form name="form" id="form">
+  <div class="admin__monitor__item">
+    住址（中文）：
+    <input
+      type="text"
+      name="adress_chi"
+      id="adress_chi"
+      value="市中心Buddy路1號"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    住址（英文）：
+    <input type="text" name="adress_eng" id="adress_eng" value="1-Buddy Road, Buddy Central" />
+  </div>
+  <div class="admin__monitor__item">
+    住址所在之地區（日文）：
+    <input
+      type="text"
+      name="adress_jp"
+      id="adress_jp"
+      value="中央区Buddy路-1"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    住址所在之地區（中文）：
+    <input
+      type="text"
+      name="district_chi"
+      id="district_chi"
+      value="${district_chi}"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    住址所在之地區（英文）：
+    <input
+      type="text"
+      name="district_eng"
+      id="district_eng"
+      value="${district_eng}"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    住址所在之地區（日文）：
+    <input type="text" name="district_jp" id="district_jp" value="${district_jp}" />
+  </div>
+  <div class="admin__monitor__item">
+    建築物名稱（中文）：
+    <input
+      type="text"
+      name="name_chi"
+      id="name_chi"
+      value="新建築"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    建築物名稱（英文）：
+    <input
+      type="text"
+      name="name_eng"
+      id="name_eng"
+      value="New Building"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    建築物名稱（日文）：
+    <input
+      type="text"
+      name="name_jp"
+      id="name_jp"
+      value="新しビール"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    所在地圖之X座標：
+    <input type="text" name="x" id="x" value="365" />
+  </div>
+  <div class="admin__monitor__item">
+    所在地圖之Y座標：
+    <input type="text" name="y" id="y" value="453" />
+  </div>
+  <div class="admin__monitor__item">
+    所在地圖之Z座標：
+    <input type="text" name="z" id="z" value="70" />
+  </div>
+  <div class="admin__monitor__item">
+    內裝（中文）：
+    <input
+      type="text"
+      name="內裝"
+      id="內裝"
+      value="有"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    內裝 （英文）：
+    <input
+      type="text"
+      name="inside"
+      id="inside"
+      value="Yes"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    內裝（日文）：
+    <input
+      type="text"
+      name="インテリア"
+      id="インテリア"
+      value="あり"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    用途（中文）：
+    <input
+      type="text"
+      name="用途"
+      id="用途"
+      value="古蹟"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    用途（英文）：
+    <input
+      type="text"
+      name="Utilization"
+      id="Utilization"
+      value="Monument"
+    />
+  </div>
+  <div class="admin__monitor__item">
+    用途（日文）
+    <input
+      type="text"
+      name="用途_jp"
+      id="用途_jp"
+      value="遺跡"
+    />
+  </div>
+  <input
+    type="button"
+    name="submit"
+    value="送出"
+    onclick="newItemBuilding('${districtId}');"
+  />
+</form>`;
+  $("#admin__monitor").append(divContent);
+}
+function showBuildingItem() {
+  $("#admin__monitor").empty();
+  var DataRef = firebase.database().ref("building");
+  var titleString = `
+  <div class="admin__monitor__title">請選擇要加入的城市建築物名內的區域：</div>
+  `;
+  $("#admin__monitor").append(titleString);
+  DataRef.once("value").then(
+    (res) => {
+      res.forEach((districtSh, buildingIndex) => {
+        let districtInfo = districtSh.child("0").val();
+        var districtTitleString = `
+        <div class="admin__monitor__chooseItem" onclick="showEditBuildingForm('${districtSh.key}','${districtInfo.district_chi}','${districtInfo.district_eng}','${districtInfo.district_jp}')">${districtInfo.district_chi}</div>
+        `;
+        $("#admin__monitor").append(districtTitleString);
+      });
+    },
+    (rej) => {
+      console.log(rej);
+    }
+  );
 }
 async function newItemResident() {
   var DataRef = firebase.database().ref("resident");
@@ -347,7 +623,7 @@ function choosePage(choice) {
       </div>
       <div
         class="admin__choosePage__editDatabase__item"
-        onclick="newItemBuilding()"
+        onclick="showBuildingItem()"
       >
         增加新資料
       </div>
